ef537f91ef4972d75dfedbcac588060501667344
Rexzy Febriano Chasan <rexzy.febrianochasan@student.upj.ac.id>
2024-11-18 14:39:15 +0700

    Add files via upload

---
+++ Pertemuan 9/routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BioController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/web', function () {
+    return "Halo, Selamat datang di tutorial belajar laravel www.malasngoding.com";
+});
+
+Route::get('/halo/{panggil}', function ($nama) {
+    return "HAAAAAI $nama";
+});
+
+Route::get('/halo/{nama}/{panjang}', function ($nama, $panjang) {
+    return "HAAAAAI $nama $panjang";
+});
+
+Route::get('/home', [BioController::class, 'nama function']);
+Route::get('/wawwy/{panggil}', [BioController::class, 'halo']);
+
+Route::get('/web', function () {
+    return view('home'); //lokasi = resource/views
+})->name('home');
+
+Route::get('/web/about', function () {
+    return view('about'); //lokasi = resource/views
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact'); //lokasi = resource/views
+})->name('contact');
+
+Route::get('/new', [PageController::class, 'home'])->name('home');
